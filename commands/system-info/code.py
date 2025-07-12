a72000253b41d04304eda97d837c1165b2e1f1c8
John Romkey <58883+romkey@users.noreply.github.com>
2025-07-12 08:04:09 -0700

    improved system-info output

--- commands/system-info/code.py
+++ commands/system-info/code.py
@@ -5,36 +5,42 @@
 import os
 import sys
 import gc
+import board
+import microcontroller
 
-cp_version = sys.implementation.version
-cp_version_str = '.'.join(str(v) for v in cp_version)
-print(f'''
-System Information:
-CircuitPython Version: {cp_version_str}
+cp_info = os.uname().version.split(' on ')
+print(f'''CircuitPython Version: {cp_info[0]}
+CircuitPython Date: {cp_info[1]}
 ''')
 
+print(os.uname().machine)
+print(board.board_id)
+
 try:
-    import board
+# SPDX-FileCopyrightText: 2020 anecdata
+#
+# SPDX-License-Identifier: MIT
 
-    print(f"Board {board.board_id}")
+# from https://gist.github.com/anecdata/1c345cb2d137776d76b97a5d5678dc97
+    for pin in dir(microcontroller.pin):
+        if isinstance(getattr(microcontroller.pin, pin), microcontroller.Pin):
+            print("".join(("microcontroller.pin.", pin, "\t")), end=" ")
+            for alias in dir(board):
+                if getattr(board, alias) is getattr(microcontroller.pin, pin):
+                    print("".join(("", "board.", alias)), end=" ")
+        print()
 
-    pins = []
-    for item in dir(board):
-        if not item.startswith('__') and not item.startswith('_') and item.isupper():
-            pins.append(item)
-    
-    if pins:
-        print(f"Available Pins: {', '.join(pins)}")
+except Exception as e:
+    print(f"Could not get pin info (Error: {e})")
 
-except (ImportError, AttributeError) as e:
-    print("Could not get pins")
-
+# SPDX-FileCopyrightText: 2025 John Romkey 
+#
+# SPDX-License-Identifier: CC0-1.0
 try:
     fs_stat = os.statvfs('/')
     flash_size = fs_stat[0] * fs_stat[2]  # Block size * Total blocks
     flash_free = fs_stat[0] * fs_stat[3]  # Block size * Free blocks
-    print(f'''
-Flash Size: {flash_size} bytes
+    print(f'''Flash Size: {flash_size} bytes
 Flash Free: {flash_free} bytes
 ''')
 except (OSError, AttributeError) as e:
@@ -45,8 +51,7 @@ try:
     mem_free = gc.mem_free()
     mem_alloc = gc.mem_alloc()
     mem_total = mem_free + mem_alloc
-    print(f'''
-Memory Total: {mem_total} bytes
+    print(f'''Memory Total: {mem_total} bytes
 Memory Free: {mem_free} bytes
 ''')
 except Exception as e:
@@ -57,8 +62,7 @@ try:
 
     radio = wifi.radio
     if radio.connected:
-        print(f'''
-Connected to: {radio.ap_info.ssid}
+        print(f'''Connected to: {radio.ap_info.ssid}
 RSSI: {radio.ap_info.rssi} dBm
 Channel: {radio.ap_info.channel}
 IP Address: {radio.ipv4_address}
@@ -68,4 +72,3 @@ IP Address: {radio.ipv4_address}
 
 except (ImportError, AttributeError):
     print("No wifi")
-
